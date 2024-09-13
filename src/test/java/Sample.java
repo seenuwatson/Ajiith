ec58c0b5a9511a8bde803a88c3222da7779dce27
seenuwatson <seeniwatson2000@gmail.com>
2024-09-12 23:53:45 -0400

    hello

---
+++ src/test/java/Sample.java
@@ -0,0 +1,16 @@
+
+public class Sample {
+
+	public void method() {
+		System.out.println("Helllo");
+	}
+	public void method2() {
+		System.out.println("Helllo");
+	}
+
+	public void method3() {
+		System.out.println("Helllo");
+	}
+	
+
+}
